63f47e60b569e4680e4a81b72d283bfba85957b0
bluefantasy728 <jiangguoxi728@hotmail.com>
2018-02-26 07:15:27 +0800

    20180226 07:13 at home

--- client/app.js
+++ client/app.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader' // eslint-disable-line
-import App from './App.jsx'
+import App from './views/App'
 
 // ReactDOM.hydrate( //这里的hydrate代替render，是因为在react16版本之后，如果是服务端渲染，则使用hydrate
 // react会对比服务端和客户端的代码的差别，如果有差别，会认为服务端的代码有问题，则会使用客户端的内容替换服务端内容
@@ -15,7 +16,9 @@ const render = (Component) => {
 	const myRender = module.hot ? ReactDOM.render : ReactDOM.hydrate
 	myRender(
 		<AppContainer>
-			<Component />
+			<BrowserRouter>
+				<Component />
+			</BrowserRouter>
 		</AppContainer>,
 		ndRoot,
 	)
@@ -24,8 +27,8 @@ const render = (Component) => {
 render(App)
 
 if (module.hot) {
-	module.hot.accept('./App.jsx', () => {
-		const NextApp = require('./App.jsx').default
+	module.hot.accept('./views/App.jsx', () => {
+		const NextApp = require('./views/App.jsx').default
 		render(NextApp)
 	})
 }
---
+++ client/views/topic_detail/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TopicDetail() {
+	return (
+		<div>topic detail</div>
+	)
+}
